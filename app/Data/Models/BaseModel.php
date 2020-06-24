fd515550e2c39a74bc150ea7d532bdafe647c4f9
BMasquio <brunomasquio@gmail.com>
2020-06-24 20:51:36 +0000

    Fix base

--- app/Data/Models/BaseModel.php
+++ app/Data/Models/BaseModel.php
@@ -1,60 +1,11 @@
 <?php
 namespace App\Data\Models;
 
-use App\Data\Presenters\BasePresenter;
 use Illuminate\Database\Eloquent\Model;
-use McCool\LaravelAutoPresenter\Facades\AutoPresenter;
-use McCool\LaravelAutoPresenter\HasPresenter;
 use OwenIt\Auditing\Auditable as AuditableTrait;
 use OwenIt\Auditing\Contracts\Auditable;
 
-abstract class BaseModel extends Model implements HasPresenter, Auditable
+abstract class BaseModel extends Model implements Auditable
 {
     use AuditableTrait;
-
-    /**
-     * @var array
-     */
-    protected $dataTypes = [];
-
-    /**
-     * @var array
-     */
-    protected $presenters = [];
-
-    /**
-     * @param $column
-     *
-     * @return mixed
-     */
-    public static function getDataTypeOf($column)
-    {
-        $model = new static();
-
-        return collect($model->dataTypes)->get($column);
-    }
-
-    /**
-     * @return string
-     */
-    public function getPresenterClass()
-    {
-        return BasePresenter::class;
-    }
-
-    /**
-     * @return array
-     */
-    public function attributesToArray()
-    {
-        $attributes = parent::attributesToArray();
-
-        $decorated = AutoPresenter::decorate($this);
-
-        foreach ($this->presenters as $key) {
-            $attributes[$key] = $decorated->$key;
-        }
-
-        return $attributes;
-    }
 }
