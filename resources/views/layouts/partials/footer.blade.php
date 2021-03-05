217397965337dade7cb919eaa2a90fb5268ed080
Breno Trengrouse <breno@gmail.com>
2021-03-05 13:27:55 -0300

    BOOTSTRAP MIGRATION - Fix footer responsive colunms

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="footer">
     <div class="container">
-        <div class="row d-flex align-items-center mt-4">
-            <div class="footer-01 col-md-4 pl-4 pl-5">
+        <div class="row d-flex align-items-center mt-4 mt-lg-0">
+            <div class="footer-01 col-md-12 col-lg-4 pl-4 pl-5">
                 <img src="/images/parla-rj-horizontal-v2.svg">
                 <p class="publication-name">Publicação da Subdiretoria-Geral de Comunicação Social da Assembleia Legislativa do Estado do Rio de Janeiro
                 </p>
             </div>
-            <div class="col-md-8 expediente">
+            <div class="text-center text-lg-left col-md-12 col-lg-8 expediente">
                 {!! $editorial ?? '' !!}
             </div>
         </div>
@@ -14,13 +14,13 @@
         <hr class="mt-4">
 
         <div class="row d-flex align-items-center bd-highlight mb-3 text-white">
-            <div class="col-12 col-sm-4 col-md-4 ">
-
+            <div class="col-12 col-md-8 offset-md-2 offset-lg-0 col-lg-4">
                 <a href="//www.alerj.rj.gov.br/" target="_blank">
                     <img src="/images/ALERJ_NOVO_horizontal-branco.png" class="img-fluid">
                 </a>
             </div>
-            <div class="col-12 col-sm-4 col-md-8 text-white">
+
+            <div class="col-12 col-md-8 offset-md-2 offset-lg-0 col-lg-8 text-white">
                 <div class="row mt-4">
                     <div class="col-lg-6 endereco">
                         <p>Rua Primeiro de março, s/n<br>
@@ -29,8 +29,8 @@
                         </p>
                     </div>
                     <div class="col-lg-6 text-white">
-                        <div class="social row">
-                            <div class="col-2"><a href="https://www.facebook.com/assembleiaRJ" target="_blank"><i class="fab fa-facebook"></i></a></div>
+                        <div class="social row mt-4 mb-5 m-lg-0">
+                            <div class="col-2 offset-1"><a href="https://www.facebook.com/assembleiaRJ" target="_blank"><i class="fab fa-facebook"></i></a></div>
                             <div class="col-2"><a href="https://twitter.com/alerj" target="_blank"><i class="fab fa-twitter"></i></a></div>
                             <div class="col-2"><a href="https://instagram.com/instalerj/" target="_blank"><i class="fab fa-instagram"></i></a></div>
                             <div class="col-2"><a href="https://www.youtube.com/user/dcsalerj" target="_blank"><i class="fab fa-youtube"></i></a></div>
